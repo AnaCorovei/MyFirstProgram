95e9ba431f9cb64dde625266313cfb8d0bc2dcda
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-19 09:49:17 +0300

    WriteLine/ReadLine

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,7 +6,20 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello evvddfvdvfdfdvdvdvderybody!");
+            int latime = 2;
+            int lungime = 3;
+
+            Console.WriteLine("Acest program va calcula aria unei camere");
+
+            Console.WriteLine("Introduceti latimea: ");
+            latime = int.Parse(Console.ReadLine()); //citire de la tastatura
+
+            Console.WriteLine("Introduceti lungimea: ");
+            lungime = int.Parse(Console.ReadLine()); //citire de la tastatura
+
+            int ariaCamerei = latime * lungime;
+
+            Console.WriteLine("Aria dreptunghiului este" + ariaCamerei);
         }
     }
 }
